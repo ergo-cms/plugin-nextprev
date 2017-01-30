7f210cbe18d44968dd5c18b1bc9844cd7dd51921
cmroanirgo <cmroanirgo@users.noreply.github.com>
2017-01-30 21:10:39 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# PrevNext Ergo-CMS plugin
+
+This plugin provides access to previous and next pages of a particular type. Most commonly, this would be use to generate Next and Prev buttons for a user to navigate.
+
+## Usage Sample
+
+All of the themes provided by ergo-cms support this plugin automatically in the `post.html` layout. The basic format to build such support is:
+
+
+```
+{{#has_nextprev}}<!--nextprev plugin-->
+<div>
+{{#prev}}
+	<a href="/{{uri}}" class="button">&lt;&lt; Previous</a>
+{{/prev}}
+{{#next}}
+	<a href="/{{uri}}" class="button">Next &gt;&gt;</a>
+{{/next}}
+</div>
+{{/has_nextprev}}
+```
+
+Note that the `{{#prev}}` and `{{#next}}` sections contain complete information about the previous/next post. As such, you can use any field available:
+
+
+```
+{{#has_nextprev}}<!--nextprev plugin-->
+<div>
+{{#prev}}
+	<a href="/{{uri}}"><div class="post-preview left" style="background-image:url(/images/{image})">
+		<h2>{{title}}</h2>
+		<h3>{{subtitle || metadesc}}</h3>
+		<p class="meta">Posted by {{author}} on {{date.formatted}}</p>
+		<p class="intro">{{short_content}}...</p>
+	</div></a>
+{{/prev}}
+{{#next}}
+	<a href="/{{uri}}"><div class="post-preview right" style="background-image:url(/images/{image})">
+		<h2>{{title}}</h2>
+		<h3>{{subtitle || metadesc}}</h3>
+		<p class="meta">Posted by {{author}} on {{date.formatted}}</p>
+		<p class="intro">{{short_content}}...</p>
+	</div></a>
+{{/next}}
+</div>
+{{/has_nextprev}}
+```
+
---
+++ plugin.ergo.js
@@ -0,0 +1,59 @@
+
+
+var l = function() { } //function(str) { console.log('next/prev: '+str); }
+
+function _find(data, direction) {
+	var uri = data.uri;
+	var post_type = data.post_type;
+	if (!uri || !post_type) {
+		l(uri + ' not found: post_type='+post_type)
+		return null;
+	}
+	//l('data is:\n' + (require('util').inspect(data, {colors:true})))
+
+	var all = data[post_type]; // this relies upon collate plugin
+	if (!all) {
+		l('list of post_types not found for ' + uri)
+		return null;
+	}
+
+	var idx = all.findIndex(function(p) { return p.uri == uri; })
+	if (idx<0) {
+		l(uri + ' not found in current list')
+		return null;
+	}
+
+	idx += direction;
+	if (idx<0 || idx>=all.length) {
+		l(uri + ' + ' + direction +' == bof/eof')
+		return null;
+	}
+	var post = all[idx];
+	l(uri + ' + ' + direction + ' ==> ' + post.uri)
+	post.isFirst = idx===0; // stop calling back into our 'isFirst/isLast', by providing the extra info now, directly
+	post.isLast = idx===all.length-1;
+	return post;
+}
+
+
+module.exports = {
+	name: "Ergo Next/Prev Plugin",
+	url: "https://github.com/ergo-cms/plugin-nextprev",
+	active: true,
+
+	default_fields: {
+		has_nextprev: true, // a simple signal to themes that we're available
+		next: function() { return _find(this, 1); },
+		prev: function() { return _find(this, -1); },
+		isFirst: function() { 
+			var p = _find(this, 0); 
+			return !!p ? p.isFirst : false;
+		},
+		isLast: function() { 
+			var p = _find(this, 0); 
+			return !!p ? p.isLast : false;
+		},
+	}
+}
+
+
